872a0cd76a482bcf965d5d5d22e43797b111cf4c
Vladimir Alexeev <9141778236@mail.ru>
2024-09-20 15:06:49 +1000

    v1.1.0   first release

--- create_client.sh
+++ create_client.sh
@@ -1,39 +1,100 @@
+#!/bin/bash
 # сгенерируем приватный ключ
 
-DEBUG_LEVEL=2
+WG_PRIVATE_KEY_CLIENT=''
+WG_PUBLIC_KEY_CLIENT=''
+WG_PSK_KEY_CLIENT=''
+WG_PUBLIC_KEY_SERVER=''
+WG_IP_CLIENT=''
+WG_DNS='1.1.1.1'
+WG_ENDPOINT='77.105.139.99:51820'
+WG_ALLOWED_IPS='0.0.0.0/0, ::0/0'
+WG_MASK_NET_CLIENT=24
 
 help(){
+  # shellcheck disable=SC2016
+  ts='dt="$(date +"%Y%m%d-%H%M%S")"; name="${name:=client-${dt}}"'
+  _dt="$(date +"%Y%m%d-%H%M%S")"
+  echo "=========================== $name"
   echo "
   Использование:
     create-client.sh command options
+    create-client.sh create-key options
     Команды:
     create-key  - создать ключи для клиента Wireguard
-    qr          - создать файл QR-код конфигурации клиента для подключения в приложении WireGuard Android
-    create-conf - создать файл конфигурации клиента на сервере WireGuard
-    all         - выполнить все задачи по порядку: create-key, сreate-conf, qr
+        Используемые опции:
+          -n --name <STR>       - на оcновании на основании этого параметра будут сформированы имена файлов ключей по-умолчанию,
+                                  если отсутствуют соответствующие опции.
+                                  По-умолчанию: вычисляется как $ts,
+                                  например: \"client-${_dt}\"
+          -u, --key-pub <STR>   - имя файла с публичным ключом
+                                  По-умолчанию: вычисляется как $(echo -n '${name}-pub.key'),
+                                  например: \"${name}-pub.key\"
+          -k  --key-priv <STR>  - имя файла с приватным ключом. Создается во время create-key. Для чтения в других action.
+                                  По-умолчанию: вычисляется как $(echo -n '${name}-private.key'),
+                                  например: \"${name}-private.key\"
+          -p, --key-psk <STR>   - имя файла с Pre-Shared Key
+                                  По-умолчанию: вычисляется как $(echo -n '${name}-psk.key'),
+                                  например: \"${name}-psk.key\"
 
-    Опции для команд:
-    -k  --key-priv              - имя файла с приватным ключом. Создается во время create-key. Для чтения в других action.
-    -p, --key-psk               - имя файла с Pre-Shared Key
-    -u, --key-pub               - имя файла с публичным ключом
-          Эти опции используются во время create-key для создания файлов.
-          Для чтения в других action.
-        
-    -c, --conf-client           - имя файла с конфигурацией клиента
-    -s  --conf-server           - имя файла с конфигурацией сервера
-          Эти опции используются во время create-conf для создания файлов.
-          Для чтения в других action (qr).
-        
-    -q, --qr                    - имя файла с QR кодом
-          Эта опция используется во время qr для создания файла.
+    qr          - создать файл QR-код конфигурации клиента для подключения в приложении WireGuard Android
+        Используемые опции:
+          -n --name         - на оcновании на основании этого параметра будет сформировано имя файла конфигурации по-умолчанию,
+                              если отсутствует опция --conf-client
+                              По-умолчанию: вычисляется как $ts,
+                              например: \"client-${_dt}\"
+          -c --conf-client  - на оcновании этого файла и будет сформирован QR-код
+                              По-умолчанию: вычисляется как $(echo -n '${name}-client.conf'),
+                              например: \"${name}-client.conf\"
+
+    create-conf - создать файл конфигурации клиента на сервере WireGuard
+        Используемые опции:
+          -n --name <STR>         - на оcновании на основании этого параметра будут сформированы имена файлов конфигураций по-умолчанию,
+                                    если отсутствуют соответствующие опции.
+                                    По-умолчанию: вычисляется как $ts,
+                                    например: \"client-${_dt}\"
+          -u, --key-pub <STR>   - имя файла с публичным ключом
+                                  По-умолчанию: вычисляется как $(echo -n '${name}-pub.key'),
+                                  например: \"${name}-pub.key\"
+          -k  --key-priv <STR>  - имя файла с приватным ключом. Создается во время create-key. Для чтения в других action.
+                                  По-умолчанию: вычисляется как $(echo -n '${name}-private.key'),
+                                  например: \"${name}-private.key\"
+          -p, --key-psk <STR>   - имя файла с Pre-Shared Key
+                                  По-умолчанию: вычисляется как $(echo -n '${name}-psk.key'),
+                                  например: \"${name}-psk.key\"
+          -c, --conf-client <STR> - имя файла с конфигурацией клиента
+                                    По-умолчанию: вычисляется как $(echo -n '${name}-client.conf'),
+                                    например: \"${name}-client.conf\"
+          -s  --conf-server <STR> - имя файла с конфигурацией сервера
+                                    По-умолчанию: вычисляется как $(echo -n '${name}-server.conf'),
+                                    например: \"${name}-server.conf\"
+          -f, --file_pubkey_server <STR>- имя файла с публичным ключом сервера WireGuard
+                                          По-умолчанию: etc/wireguard/keys/server_pub
+          -i, --ip-client         - ip адрес назначаемый клиенту
+                                    По-умолчанию: ''
+          -m, --mask-client         - ip адрес назначаемый клиенту
+                                    По-умолчанию: ''
+              --dns-client        - DNS сервер назначаемый клиенту
+                                    По-умолчанию: '1.1.1.1'
+          '-e' | '--endpoint'     - EndPoint для подключения к серверу
+                                    По-умолчанию: '77.105.139.99:51820'
+
+    all       - выполнить все задачи по порядку: create-key, сreate-conf, qr
+
+    Опции общие:
+    -d, --debug                 - флаг вывода отладочных сообщений
+        --debug-level <Num>     - не используется (пока)
+                                  По-умолчанию: 2
+    ${0}
+    ${0} qr
+    ${0} qr -c /etc/wireguard/config/client.conf
   "
 }
 
 _debug(){
-  level=$2
   level=${level:=$DEBUG_LEVEL}
-  if [[ $DEBUG -ne "0" ]]; then
-    echo "debug(l${level})::: $1" > 2
+  if [[ $DEBUG -ne 0 ]]; then
+    echo "debug(l-${level})::: $1" >&2
   fi
 }
 
@@ -43,18 +104,58 @@ _startswith() {
   echo "$_str" | grep -- "^$_sub" >/dev/null 2>&1
 }
 
+_create_key(){
+  # _create_key
+  _debug "##### Создание ключей (private, PSK, public) для клиента"
+  # сгенерируем приватный ключ
+  wg genkey | tee "$file_key_priv" > /dev/null
+  # сгенерируем PSK ключ
+  wg genpsk | tee "$file_key_psk" > /dev/null
+  # сгенерируем публичный ключ
+  wg pubkey < "$file_key_priv"| tee "$file_key_pub" > /dev/null
+}
 
-nm="$1"
-dt="$(date +"%Y%m%d-%H%M%S")"
-name="${nm:=client-$dt}"
+_create_conf(){
+  # _create_conf
+  # доинициализировать переменные для шаблонов
+  WG_PRIVATE_KEY_CLIENT=$(sed -En 's/(.*)$/\1/p' "$file_key_priv")
+  WG_PSK_KEY_CLIENT=$(sed -En 's/(.*)$/\1/p' "$file_key_psk")
+  WG_PUBLIC_KEY_CLIENT=$(sed -En 's/(.*)$/\1/p' "$file_key_pub")
+  WG_PUBLIC_KEY_SERVER=$(sed -En 's/(.*)$/\1/p' "$file_pubkey_server")
 
-declare -a array_env
+  _debug "##### Создание файлов с конфигурациями для клиента и для сервера"
+  _debug "WG_IP_CLIENT: ${WG_IP_CLIENT}"
+  _debug "WG_MASK_NET_CLIENT: ${WG_MASK_NET_CLIENT}"
+  _debug "WG_DNS: ${WG_DNS}"
+  _debug "WG_ENDPOINT: ${WG_ENDPOINT}"
+  _debug "WG_ALLOWED_IPS: ${WG_ALLOWED_IPS}"
+  _debug "WG_PRIVATE_KEY_CLIENT: ${WG_PRIVATE_KEY_CLIENT}"
+  _debug "WG_PSK_KEY_CLIENT: ${WG_PSK_KEY_CLIENT}"
+  _debug "WG_PUBLIC_KEY_CLIENT: ${WG_PUBLIC_KEY_CLIENT}"
+  _debug "WG_PUBLIC_KEY_SERVER: ${WG_PUBLIC_KEY_SERVER}"
+
+  eval "echo \"$(cat ./template-client.conf)\" > \"${file_conf_client}\""
+  # генерируем файл для добавления к конфигу сервера
+  eval "echo \"$(cat ./template-server.conf)\" >> \"${file_conf_server}\""
+}
+
+_create_qr(){
+  # _create_qr "$name" "$file_conf_client" "$file_qr_code"
+  _debug "##### Создание файла с QR кодом"
+  _debug "##### name: $name"
+  _debug "##### file_conf_client: $file_conf_client"
+  _debug "##### file_qr_code: $file_qr_code"
+  qrencode -t png -o "$file_qr_code" -r "$file_conf_client"
+}
+
+#============================================================================================
+#============================================================================================
+#============================================================================================
+#declare -a array_env
 # массив всех доступных action
-actions=(create-key qr create-conf all)
+#actions=(create-key qr create-conf all)
 
-
-# новый разбор аргументов. Теперь если первый аргумент не начинается с '-', то это action, осталное опции для action
-# action: add, backup, delete, decode-file, encode-file, export
+# новый разбор аргументов. Теперь если первый аргумент не начинается с '-', то это action, остальное опции
 #args=$@
 if ! _startswith "$1" '-'; then
   # здесь 1-й аргумент не начинается с '-', т.е. здесь первый аргумент, все остальное опции
@@ -64,29 +165,42 @@ else
   action='qr'
 fi
 
-if ! args=$(getopt -u -o 'k:p:u:c:s:q:d' --long 'key-priv:,key-psk:,key-pub:,conf-client:,conf-server:,qr:debug,debug-level:' -- "$@"); then
+if ! args=$(getopt -u -o 'hk:p:u:c:s:q:n:df:i:e:a:m:r' --long 'help,key-priv:,key-psk:,key-pub:,conf-client:,conf-server:,qr:,name:,debug,debug-level:,file-pubkey-server:,ip-client:,dns-client:,endpoint:,allow-addr:,mask-client:,restart-server' -- "$@"); then
   help;
   exit 0;
 fi
 
-set -- $args
-_debug "$args"
+# DEFAULT значения
+name="${name:=client-$(date +"%Y%m%d-%H%M%S")}"
+
+set -- ${args}
+#echo "$args"
 i=0
 for i; do
   case "$i" in
+    '-h' | '--help')        help; exit 0;;
     '-k' | '--key-priv')    file_key_priv=${2};     shift 2;;
     '-p' | '--key-psk')     file_key_psk=${2};      shift 2;;
     '-u' | '--key-pub')     file_key_pub=${2};      shift 2;;
     '-c' | '--conf-client') file_conf_client=${2};  shift 2;;
     '-s' | '--conf-server') file_conf_server=${2};  shift 2;;
-    '-q' | '--qr')          file_qr_code=${2};      shift 2;;
+    '-q' | '--qr-file')     file_qr_code=${2};      shift 2;;
+    '-n' | '--name')        name=${2};              shift 2;;
     '-d' | '--debug')       DEBUG=1;                shift;;
-    '--debug-level')        DEBUG_LEVEL="${1}";     shift 2;;
+    '--debug-level')        DEBUG_LEVEL=${2};       shift 2;;
+    '-f' | '--file-pubkey-server') file_pubkey_server=${2}; shift 2;;
+    '-i' | '--ip-client')   WG_IP_CLIENT="${2}";    shift 2;;
+    '-m' | '--mask-client') WG_MASK_NET_CLIENT="${2}"; shift 2;;
+    '--dns-client')         WG_DNS="${2}";          shift 2;;
+    '-e' | '--endpoint')    WG_ENDPOINT="${2}";     shift 2;;
+    '-a' | '--allow-addr')  WG_ALLOWED_IPS="${2}";  shift 2;;
+    '-r' | '--restart-server') restart_server=1;    shift;;
   esac
 done
-echo $DEBUG
 
-_debug "debug: $DEBUG"
+#
+file_pubkey_server="${file_pubkey_server:=/etc/wireguard/keys/server_pub}"
+pubkey_server=$(sed -En 's/(.*)$/\1/p' "$file_pubkey_server")
 
 file_key_priv="${file_key_priv:=${name}-private.key}"
 file_key_psk="${file_key_psk:=${name}-psk.key}"
@@ -94,9 +208,18 @@ file_key_pub="${file_key_pub:=${name}-public.key}"
 
 file_conf_client="${file_conf_client:=${name}-client.conf}"
 file_conf_server="${file_conf_server:=${name}-server.conf}"
+#file_conf_server="${file_conf_server:=/etc/wireguard/wg0.conf}"
 
 file_qr_code="${file_qr_code:=${name}-qr.png}"
+restart_server="${restart_server:=0}"
 
+DEBUG_LEVEL="${DEBUG_LEVEL:=2}"
+_debug "name: $name"
+_debug "file_pubkey_server: $file_pubkey_server"
+_debug "pubkey_server: $pubkey_server"
+
+_debug "debug: $DEBUG"
+_debug "debug_level: $DEBUG_LEVEL"
 _debug "action: ${action}"
 _debug "file_key_priv: ${file_key_priv}"
 _debug "file_key_psk: ${file_key_psk}"
@@ -107,14 +230,25 @@ _debug "file_conf_server: ${file_conf_server}"
 
 _debug "file_qr_code: ${file_qr_code}"
 
-exit
-
-#wg genkey | tee "${name}_private.key"
-#wg genpsk | tee "${name}_psk.key"
-# сгенерируем публичный ключ
-#cat "${name}_private.key" | wg pubkey | tee "${name}_public.key"
-
-# from https://timeweb.cloud/tutorials/network-security/wireguard-na-svoem-servere
-#wg genkey | tee /etc/wireguard/user1_privatekey | wg pubkey | tee /etc/wireguard/user1_publickey
-# generate QR code
-qrencode -t png -o "${name}-qr.png" -r  "${name}.conf"
+#exit
+case "$action" in
+  "create-key")
+      _create_key "$name" "$file_key_priv" "$file_key_psk" "$file_key_pub"
+      ;;
+  "create-conf")
+      _create_conf
+      ;;
+  "qr")
+      _create_qr
+      ;;
+  "all")
+      _create_key "$name"
+      _create_conf "$name" "$file_key_priv"
+      _create_qr "$name" "$file_qr_code"
+      ;;
+esac
+if [[ $restart_server -ne 0 ]]; then
+  _debug "Restart WireGuard server..."
+  systemctl restart wg-quick@wg0.service
+  _debug "Ending restart..."
+fi
