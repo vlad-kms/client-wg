2540f6225c5cffadcde20211a793b6930370419d
Vladimir Alexeev <9141778236@mail.ru>
2024-09-20 21:35:08 +1000

    v1.1.1 fix

--- create_client.sh
+++ create_client.sh
@@ -11,11 +11,13 @@ WG_ENDPOINT='77.105.139.99:51820'
 WG_ALLOWED_IPS='0.0.0.0/0, ::0/0'
 WG_MASK_NET_CLIENT=24
 
+path_files='./.out'
+
 help(){
   # shellcheck disable=SC2016
-  ts='dt="$(date +"%Y%m%d-%H%M%S")"; name="${name:=client-${dt}}"'
+  ts='dt="$(date +"%Y%m%d-%H%M%S")"; name="${name:=./.out/client-${dt}}"'
   _dt="$(date +"%Y%m%d-%H%M%S")"
-  echo "=========================== $name"
+  path_files="${path_files:=./.out}"
   echo "
   Использование:
     create-client.sh command options
@@ -26,48 +28,48 @@ help(){
           -n --name <STR>       - на оcновании на основании этого параметра будут сформированы имена файлов ключей по-умолчанию,
                                   если отсутствуют соответствующие опции.
                                   По-умолчанию: вычисляется как $ts,
-                                  например: \"client-${_dt}\"
+                                  например: \"./.out/client-${_dt}\"
           -u, --key-pub <STR>   - имя файла с публичным ключом
-                                  По-умолчанию: вычисляется как $(echo -n '${name}-pub.key'),
-                                  например: \"${name}-pub.key\"
+                                  По-умолчанию: вычисляется как $(echo -n './.out/${name}-pub.key'),
+                                  например: \"./.out/${name}-pub.key\"
           -k  --key-priv <STR>  - имя файла с приватным ключом. Создается во время create-key. Для чтения в других action.
-                                  По-умолчанию: вычисляется как $(echo -n '${name}-private.key'),
-                                  например: \"${name}-private.key\"
+                                  По-умолчанию: вычисляется как $(echo -n './.out/${name}-private.key'),
+                                  например: \"./.out/${name}-private.key\"
           -p, --key-psk <STR>   - имя файла с Pre-Shared Key
-                                  По-умолчанию: вычисляется как $(echo -n '${name}-psk.key'),
-                                  например: \"${name}-psk.key\"
+                                  По-умолчанию: вычисляется как $(echo -n './.out/${name}-psk.key'),
+                                  например: \"./.out/${name}-psk.key\"
 
     qr          - создать файл QR-код конфигурации клиента для подключения в приложении WireGuard Android
         Используемые опции:
           -n --name         - на оcновании на основании этого параметра будет сформировано имя файла конфигурации по-умолчанию,
                               если отсутствует опция --conf-client
                               По-умолчанию: вычисляется как $ts,
-                              например: \"client-${_dt}\"
+                              например: \"./.out/client-${_dt}\"
           -c --conf-client  - на оcновании этого файла и будет сформирован QR-код
-                              По-умолчанию: вычисляется как $(echo -n '${name}-client.conf'),
-                              например: \"${name}-client.conf\"
+                              По-умолчанию: вычисляется как $(echo -n './.out/${name}-client.conf'),
+                              например: \"./.out/${name}-client.conf\"
 
     create-conf - создать файл конфигурации клиента на сервере WireGuard
         Используемые опции:
           -n --name <STR>         - на оcновании на основании этого параметра будут сформированы имена файлов конфигураций по-умолчанию,
                                     если отсутствуют соответствующие опции.
                                     По-умолчанию: вычисляется как $ts,
-                                    например: \"client-${_dt}\"
+                                    например: \"./.out/client-${_dt}\"
           -u, --key-pub <STR>   - имя файла с публичным ключом
-                                  По-умолчанию: вычисляется как $(echo -n '${name}-pub.key'),
-                                  например: \"${name}-pub.key\"
+                                  По-умолчанию: вычисляется как $(echo -n './.out/${name}-pub.key'),
+                                  например: \"./.out/${name}-pub.key\"
           -k  --key-priv <STR>  - имя файла с приватным ключом. Создается во время create-key. Для чтения в других action.
-                                  По-умолчанию: вычисляется как $(echo -n '${name}-private.key'),
-                                  например: \"${name}-private.key\"
+                                  По-умолчанию: вычисляется как $(echo -n './.out/${name}-private.key'),
+                                  например: \"./.out/${name}-private.key\"
           -p, --key-psk <STR>   - имя файла с Pre-Shared Key
-                                  По-умолчанию: вычисляется как $(echo -n '${name}-psk.key'),
-                                  например: \"${name}-psk.key\"
+                                  По-умолчанию: вычисляется как $(echo -n './.out/${name}-psk.key'),
+                                  например: \"./.out/${name}-psk.key\"
           -c, --conf-client <STR> - имя файла с конфигурацией клиента
-                                    По-умолчанию: вычисляется как $(echo -n '${name}-client.conf'),
-                                    например: \"${name}-client.conf\"
+                                    По-умолчанию: вычисляется как $(echo -n './.out/${name}-client.conf'),
+                                    например: \"./.out/${name}-client.conf\"
           -s  --conf-server <STR> - имя файла с конфигурацией сервера
-                                    По-умолчанию: вычисляется как $(echo -n '${name}-server.conf'),
-                                    например: \"${name}-server.conf\"
+                                    По-умолчанию: вычисляется как $(echo -n './.out/${name}-server.conf'),
+                                    например: \"./.out/${name}-server.conf\"
           -f, --file_pubkey_server <STR>- имя файла с публичным ключом сервера WireGuard
                                           По-умолчанию: etc/wireguard/keys/server_pub
           -i, --ip-client         - ip адрес назначаемый клиенту
@@ -85,6 +87,10 @@ help(){
     -d, --debug                 - флаг вывода отладочных сообщений
         --debug-level <Num>     - не используется (пока)
                                   По-умолчанию: 2
+    -r, --restart-server        - перезапустить сервис WireGuard после выполнения действий
+    -t, --path-files            - путь где будут сохраняться выходные файлы, если они определены бех пути
+                                  По-умолчанию: ./.out
+
     ${0}
     ${0} qr
     ${0} qr -c /etc/wireguard/config/client.conf
@@ -134,9 +140,9 @@ _create_conf(){
   _debug "WG_PUBLIC_KEY_CLIENT: ${WG_PUBLIC_KEY_CLIENT}"
   _debug "WG_PUBLIC_KEY_SERVER: ${WG_PUBLIC_KEY_SERVER}"
 
-  eval "echo \"$(cat ./template-client.conf)\" > \"${file_conf_client}\""
+  eval "echo \"$(cat ./template-client.tmpl)\" > \"${file_conf_client}\""
   # генерируем файл для добавления к конфигу сервера
-  eval "echo \"$(cat ./template-server.conf)\" >> \"${file_conf_server}\""
+  eval "echo \"$(cat ./template-server.tmpl)\" >> \"${file_conf_server}\""
 }
 
 _create_qr(){
@@ -165,7 +171,7 @@ else
   action='qr'
 fi
 
-if ! args=$(getopt -u -o 'hk:p:u:c:s:q:n:df:i:e:a:m:r' --long 'help,key-priv:,key-psk:,key-pub:,conf-client:,conf-server:,qr:,name:,debug,debug-level:,file-pubkey-server:,ip-client:,dns-client:,endpoint:,allow-addr:,mask-client:,restart-server' -- "$@"); then
+if ! args=$(getopt -u -o 'hk:p:u:c:s:q:n:df:i:e:a:m:r,t:' --long 'help,key-priv:,key-psk:,key-pub:,conf-client:,conf-server:,qr:,name:,debug,debug-level:,file-pubkey-server:,ip-client:,dns-client:,endpoint:,allow-addr:,mask-client:,restart-server,path-files:' -- "$@"); then
   help;
   exit 0;
 fi
@@ -195,22 +201,73 @@ for i; do
     '-e' | '--endpoint')    WG_ENDPOINT="${2}";     shift 2;;
     '-a' | '--allow-addr')  WG_ALLOWED_IPS="${2}";  shift 2;;
     '-r' | '--restart-server') restart_server=1;    shift;;
+    '-t' | '--path-files')  path_files="$2";        shift 2;;
   esac
 done
 
-#
+if ! [ -d "$path_files" ]; then
+  mkdir "$path_files"
+fi
+
+# public key server WireGuard
 file_pubkey_server="${file_pubkey_server:=/etc/wireguard/keys/server_pub}"
 pubkey_server=$(sed -En 's/(.*)$/\1/p' "$file_pubkey_server")
 
+# файл с приватным ключом клиента
 file_key_priv="${file_key_priv:=${name}-private.key}"
-file_key_psk="${file_key_psk:=${name}-psk.key}"
-file_key_pub="${file_key_pub:=${name}-public.key}"
+#b="safsdfsdf"; aa=$(expr match "$b" '\(.*[\/].*\)'); echo $b; echo $aa; if [[ -z "$aa" ]]; then echo "not slash"; else echo "yes slash"; f
+tfn="${file_key_priv}"
+aa=$(expr "$tfn" : '\(.*[/].*\)')
+if [[ -z $aa ]]; then
+  _debug "Имя файла ${tfn} не содержит путей. Добавим к имени ${path_files}: ${path_files}/${tfn}"
+  file_key_priv="${path_files}/${tfn}"
+fi
 
+# файл с PSK ключом клиента
+file_key_psk="${file_key_psk:=${name}-psk.key}"
+tfn="${file_key_psk}"
+aa=$(expr "$tfn" : '\(.*[/].*\)')
+if [[ -z $aa ]]; then
+  _debug "Имя файла ${tfn} не содержит путей. Добавим к имени ${path_files}: ${path_files}/${tfn}"
+  file_key_psk="${path_files}/${tfn}"
+fi
+
+# файл с публичным ключом клиента
+file_key_pub="${file_key_pub:=${name}-public.key}"
+tfn="${file_key_pub}"
+aa=$(expr "$tfn" : '\(.*[/].*\)')
+if [[ -z $aa ]]; then
+  _debug "Имя файла ${tfn} не содержит путей. Добавим к имени ${path_files}: ${path_files}/${tfn}"
+  file_key_pub="${path_files}/${tfn}"
+fi
+
+# файл с конфигурацией клиента
 file_conf_client="${file_conf_client:=${name}-client.conf}"
+tfn="${file_conf_client}"
+aa=$(expr "$tfn" : '\(.*[/].*\)')
+if [[ -z $aa ]]; then
+  _debug "Имя файла ${tfn} не содержит путей. Добавим к имени ${path_files}: ${path_files}/${tfn}"
+  file_conf_client="${path_files}/${tfn}"
+fi
+# файл с конфигурацией сервера
 file_conf_server="${file_conf_server:=${name}-server.conf}"
+tfn="${file_conf_server}"
+aa=$(expr "$tfn" : '\(.*[/].*\)')
+if [[ -z $aa ]]; then
+  _debug "Имя файла ${tfn} не содержит путей. Добавим к имени ${path_files}: ${path_files}/${tfn}"
+  file_conf_server="${path_files}/${tfn}"
+fi
 #file_conf_server="${file_conf_server:=/etc/wireguard/wg0.conf}"
 
+# файл с конфигурацией клиента в формате QR-кода
 file_qr_code="${file_qr_code:=${name}-qr.png}"
+tfn="${file_qr_code}"
+aa=$(expr "$tfn" : '\(.*[/].*\)')
+if [[ -z $aa ]]; then
+  _debug "Имя файла ${tfn} не содержит путей. Добавим к имени ${path_files}: ${path_files}/${tfn}"
+  file_qr_code="${path_files}/${tfn}"
+fi
+
 restart_server="${restart_server:=0}"
 
 DEBUG_LEVEL="${DEBUG_LEVEL:=2}"
@@ -229,6 +286,9 @@ _debug "file_conf_client: ${file_conf_client}"
 _debug "file_conf_server: ${file_conf_server}"
 
 _debug "file_qr_code: ${file_qr_code}"
+
+_debug "restart_server: ${restart_server}"
+_debug "path_files: ${path_files}"
 
 #exit
 case "$action" in
