4061a2e2caa3aaa0a4d00a380160d5b57e8db94e
Vladimir Alexeev <9141778236@mail.ru>
2026-08-01 09:42:32 +1000

    v2.8.44 fix: поправил devops/*.sh

--- devops/cf-vi.sh
+++ devops/cf-vi.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+# shellcheck disable=SC2034
 # shellcheck disable=SC2317
 # shellcheck disable=SC2329
 
 dir_root="${DIR_ROOT:-..}"
-dir_temp="${DIR_TEMP:-.wg-temp/cl}"
+dir_temp="${DIR_TEMP:-.wg-temp/vi}"
 file_cfg="${FILE_CFG:-conf.conf}"
 
 _prepare() {
   force=0
-  if [ ! -f "${dir_root}/${dir_temp}/$file_cfg" ] || [ $force -eq 1 ]; then
+  if [ ! -f "${dir_root}/${dir_temp}/${file_cfg}" ] || [[ $force -eq 1 ]]; then
     echo "Создаем файл конфигурации для инсталляции ${file_cfg}"
     "${dir_root}"/wg-mgr.sh prepare \
       --debug \
@@ -18,7 +19,7 @@ _prepare() {
       --wg-path "${dir_root}/${dir_temp}" \
       --rules-iptables "${dir_root}/nftables/nft.rules" \
       --wg-nic wg0 \
-      --ip4 10.16.18.1/24 \
+      --ip4 10.16.16.1/24 \
       --dns '1.1.1.1,1.0.0.1' \
       --allowed_ips '0.0.0.0/0' \
       "$@"
@@ -34,30 +35,21 @@ _install() {
     --debug \
     --dry-run \
     --config "${dir_root}/${dir_temp}/${file_cfg}" \
-    --wg-nic wg0 \
-    --ip4 10.16.18.1/24 \
-    --dns 192.168.15.3 \
     --wg-path "${dir_root}/${dir_temp}" \
     --rules-iptables "${dir_root}/nftables/nft.rules" \
-    --option "WG_NET=10.16.18.0/24" \
+    --wg-nic wg0 \
+    --ip4 10.16.16.1/24 \
+    --dns '1.1.1.1,1.0.0.1' \
+    --option "WG_NET=10.16.16.0/24" \
     --option "NFT_COUNTER=counter; NFT_SEARCH_DOMAIN_LOCALNET=\"home.lan klinika.lan\"" \
     --option "PROVIDER_GW_MAC=" \
-    --option "NFT_NAT_NET=\"192.168.15.0/24, 192.168.16.0/24,192.168.22.0/24, 192.168.25.0/24,192.168.26.0/24\"" \
-    --option "NFT_LIST_TRUST_WAN='vi.vpn.mrovo.ru, cl-ang.vpn.mrovo.ru,v4v.vpn.mrovo.ru,fn.vpn.mrovo.ru'" \
-    --option "NFT_LIST_TRUST_VPN=\"10.16.18.2,10.16.18.3,10.16.18.4,10.16.18.5,10.16.18.6,10.16.18.7,10.16.18.254,192.168.15.0/24,192.168.16.0/24\"" \
+    --option "NFT_NAT_NET=\"10.16.19.0/24, 192.168.15.0/24, 192.168.16.0/24,192.168.22.0/24, 192.168.25.0/24,192.168.26.0/24\"" \
+    --option "NFT_LIST_TRUST_WAN='cl.vpn.mrovo.ru, cl-ang.vpn.mrovo.ru, 4v.vpn.mrovo.ru,fn.vpn.mrovo.ru'" \
     --option "NFT_LIST_VPN=" \
     --option "NFT_LIST_VPN_ONLY=" \
-    --option "NFT_LIST_INET_DROP='10.16.18.100'" \
+    --option "NFT_LIST_INET_DROP=" \
+    --option "NFT_LIST_TRUST_VPN=\"10.16.16.2,10.16.16.3,10.16.16.4,10.16.16.5,10.16.16.6,10.16.16.7,10.16.16.254,192.168.15.0/24,192.168.16.0/24\"" \
     --option "NFT_DNS_LOCALNET=192.168.15.3" \
-    --option 'NFT_MAP_FORWARD_ACL="
-        192.168.15.79 . tcp . 80 : accept,
-        192.168.15.79 . tcp . 443 : accept,
-        192.168.16.79 . tcp . 80 : accept,
-        192.168.16.79 . tcp . 443 : accept
-      "' \
-    --option 'NFT_MAP_DNAT="tcp . 443 : 192.168.15.79 . 443, tcp . 80 : 192.168.15.79 . 80, tcp . 8080 : 192.168.16.79 . 80, tcp . 8443 : 192.168.16.79 . 443"' \
-    --option 'NFT_ALLOWED_SERVICES=' \
-    --option 'NFT_ALLOWED_SERVICES_VPN="udp . 161"' \
     "$@"
 }
 
@@ -67,7 +59,7 @@ shift 1
 
 mkdir -p "${dir_root}/${dir_temp}"
 
-if [[ "$act" =~ 'p' ]]; then
+if [[ "$act" =~ p ]]; then
   echo "prepare"
   is_true=1
   _prepare "$@"
@@ -78,12 +70,10 @@ if [[ "$act" =~ i ]]; then
   _install "$@"
 fi
 if [ "$is_true" -eq 1 ]; then
-  echo 'Running script'
+  echo "Running script"
   # cat ./hand_params.conf
 else
-  echo 'Не указано что делать'
+  echo "Не указано что делать"
 fi
 
 exit 0
-
-# --option 'NFT_FILENAME_CUSTOM_RULES=custom_rules.sh' \
